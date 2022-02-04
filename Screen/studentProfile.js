f49fe4c49f3a6ce904cc11766807f03a88573c85
AlGhaliyahM <439201044@student.ksu.edu.sa>
2022-02-05 02:06:46 +0300

    Add profile screen

---
+++ Screen/studentProfile.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  useColorScheme,
+  View,
+} from 'react-native';
+import {FloatingLabelInput} from 'react-native-floating-label-input';
+import TitleStyles from '../Styles/Titles';
+import Top2Lines from '../assets/images/top2Lines.svg';
+import TopBox from '../assets/images/TopBox.svg';
+const studentProfile = () => {
+  return (
+    <SafeAreaView
+      style={{
+        backgroundColor: '#FFFFFF',
+        height: '100%',
+        // justifyContent: 'center',
+        // alignItems: 'center',
+      }}>
+      <TopBox style={[{position: 'absolute', top: 0, Right: 250}]}></TopBox>
+      <Top2Lines
+        style={[
+          TitleStyles.shadowOffset,
+          {position: 'absolute', top: 0, left: 0},
+        ]}
+      />
+      <Text style={TitleStyles.HeaderTitle}>الملف الشخصي</Text>
+    </SafeAreaView>
+  );
+};
+export default studentProfile;
--- Styles/Titles.js
+++ Styles/Titles.js
@@ -10,6 +10,15 @@ export default StyleSheet.create({
     color: '#43515F',
   },
 
+  HeaderTitle: {
+    fontFamily: 'AJannatLT-Bold',
+    fontSize: 30,
+    fontWeight: 'bold',
+    color: '#43515F',
+    marginLeft: 160,
+    marginTop: 10,
+  },
+
   // sub headings
   subTitle: {
     fontFamily: 'AJannatLT',
@@ -36,7 +45,7 @@ export default StyleSheet.create({
     borderColor: '#f8f4f4',
     flexDirection: 'row-reverse',
     backgroundColor: '#f8f4f4',
-    marginBottom:10
+    marginBottom: 10,
   },
   categoryText: {
     textAlign: 'right',
@@ -47,52 +56,50 @@ export default StyleSheet.create({
   Button: {
     backgroundColor: '#F5C5AD',
     marginTop: 20,
-    marginBottom:10,
-    borderRadius:13,
-    shadowColor: "#00000",
-            shadowOffset: {
-	          width: 0,
-	          height: 4,
-              },
-            shadowOpacity: 0.25,
-            shadowRadius: 9,
-            elevation: 9,
+    marginBottom: 10,
+    borderRadius: 13,
+    shadowColor: '#00000',
+    shadowOffset: {
+      width: 0,
+      height: 4,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 9,
+    elevation: 9,
   },
 
-  ButtonText:{
-     fontFamily: 'AJannatLT-Bold',
-     fontSize:28,
-     color:"#43515F",
-     textAlign:'center'
+  ButtonText: {
+    fontFamily: 'AJannatLT-Bold',
+    fontSize: 28,
+    color: '#43515F',
+    textAlign: 'center',
+  },
 
-
-   },
-
-    modalContent: {
+  modalContent: {
     margin: 20,
     marginBottom: 'auto',
     marginTop: 'auto',
-    backgroundColor: "white",
+    backgroundColor: 'white',
     borderRadius: 20,
     padding: 35,
-    shadowColor: "#000",
+    shadowColor: '#000',
     shadowOffset: {
       width: 0,
-      height: 2
+      height: 2,
     },
     shadowOpacity: 0.25,
     shadowRadius: 4,
-    elevation: 5
+    elevation: 5,
   },
 
-  shadowOffset:{
-    shadowColor: "#00000",
-            shadowOffset: {
-	          width: 0,
-	          height: 4,
-              },
-            shadowOpacity: 0.25,
-            shadowRadius: 9,
-            elevation: 9,
-  }
+  shadowOffset: {
+    shadowColor: '#00000',
+    shadowOffset: {
+      width: 0,
+      height: 4,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 9,
+    elevation: 9,
+  },
 });
