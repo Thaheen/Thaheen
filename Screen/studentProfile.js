bed04027abacdc69c139a3a677bfe2a012af526b
AlGhaliyahM <439201044@student.ksu.edu.sa>
2022-02-07 13:19:36 +0300

    Contain error in floating label

--- Screen/studentProfile.js
+++ Screen/studentProfile.js
@@ -10,10 +10,10 @@ import {
   useColorScheme,
   View,
 } from 'react-native';
-
 import TitleStyles from '../Styles/Titles';
 import Top2Lines from '../assets/images/top2Lines.svg';
 import TopBox from '../assets/images/TopBox.svg';
+import {FloatingLabelInput} from 'react-native-floating-label-input';
 const studentProfile = () => {
   return (
     <SafeAreaView
@@ -31,8 +31,48 @@ const studentProfile = () => {
         ]}
       />
       <Text style={TitleStyles.HeaderTitle}>الملف الشخصي</Text>
+      <View>
+        <FloatingLabelInput
+          label="الاسم الأول"
+          staticLabel
+          //value={NewFirstName}
+          hintTextColor={'black'}
+          editable={true}
+          selectTextOnFocus={true}
+          //hint={userFirstName}
+          //onChangeText={text => setNewFirstName(text)}
+          caretHidden={false}
+          containerStyles={{
+            borderWidth: 0.76,
+            paddingHorizontal: 8,
+            bottom: -3,
+            textAlign: 'right',
+            borderColor: 'gray',
+            borderRadius: 8,
+            height: 50,
+          }}
+          customLabelStyles={{
+            colorFocused: 'black',
+            fontSizeFocused: 12,
+          }}
+          labelStyles={{
+            backgroundColor: '#f2f2f2',
+            paddingHorizontal: 8,
+            marginLeft: 200,
+            fontFamily: 'AJannatLT',
+          }}
+          inputStyles={{
+            color: 'black',
+            paddingHorizontal: 10,
+            fontFamily: 'AJannatLT',
+            fontSize: 14,
+            color: '#002B3E',
+            textAlign: 'right',
+            fontWeight: 'bold',
+          }}
+        />
+      </View>
     </SafeAreaView>
   );
 };
-
 export default studentProfile;
