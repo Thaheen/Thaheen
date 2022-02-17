83e3f8d1aa040f070d9be7a1beee91cc2d154873
AlGhaliyahM <439201044@student.ksu.edu.sa>
2022-02-17 10:45:58 +0300

    Adding the instructor profile in stack

---
+++ Screen/InstructorProfile.js
@@ -0,0 +1,61 @@
+import React, {useState, useEffect} from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  useColorScheme,
+  View,
+  I18nManager,
+} from 'react-native';
+import TitleStyles from '../Styles/Titles';
+import RTLlayout from '../Styles/RTLlayout';
+import Top2Lines from '../assets/images/top2Lines.svg';
+import TopBox from '../assets/images/TopBox.svg';
+import AnimalPicker from '../Screen/AnimalPicker.js';
+import auth from '@react-native-firebase/auth';
+import firestore from '@react-native-firebase/firestore';
+import BackButton from '../Components/BackButton.js';
+
+
+const InstructorProfile = ({navigation, route}) => {
+
+    //VIEW , EDIT PROFILE 
+    
+  const user = auth().currentUser;
+
+  const [fullName, setFullName] = useState('');
+  const [Email, setEmail = useState('');
+  const [PhoneNum, setPhoneNum] = useState('');
+ 
+
+  useEffect(() => {
+    const InstructorInfo = firestore()
+      .collection('Instructors Accounts')
+    //  .doc(route.params.studentID)
+      .onSnapshot(snapshot => {
+        setFullName(snapshot.data().Fullname);
+        setEmail(snapshot.data().email);
+        setPhoneNum(snapshot.data().Phone);
+      });
+    return InstructorInfo;
+  }, []);
+
+  return (
+    <SafeAreaView
+      style={{
+        backgroundColor: '#FFFFFF',
+        height: '100%',
+        // justifyContent: 'center',
+        // alignItems: 'center',
+      }}>
+      <Text> HERE IS THE INSTRUCTOR PROFILE</Text>
+     
+      
+    </SafeAreaView>
+  );
+};
+export default InstructorProfile;
--- auth/InstructorStack.js
+++ auth/InstructorStack.js
@@ -1,20 +1,27 @@
 import * as React from 'react';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
 
-import {default as Home} from '../Screen/Home.js'
-
+import {default as Home} from '../Screen/Home.js';
+import {default as InstructorProfile} from '../Screen/InstructorProfile.js';
 const Stack = createNativeStackNavigator();
 
 const InstructorStack = () => {
-
-//Please put  here all the screens related to instructor homepage, classroom..etc
+  //Please put  here all the screens related to instructor homepage, classroom..etc
   return (
-      <Stack.Navigator initialRouteName="Home">
-        <Stack.Screen name="Home" component={Home} options={{headerShown: false}}/>
-      </Stack.Navigator>
+    <Stack.Navigator initialRouteName="Home">
+      <Stack.Screen
+        name="Home"
+        component={Home}
+        options={{headerShown: false}}
+      />
+
+      <Stack.Screen
+        name="InstructorProfile"
+        component={InstructorProfile}
+        options={{headerShown: false}}
+      />
+    </Stack.Navigator>
   );
-}
-
-
+};
 
 export default InstructorStack;
