401b32449485d0bad6458fda1cd8a60003dbf2a1
munirra <monirah.alessa@gmail.com>
2022-03-13 19:02:59 +0300

    Update MemorizationSession.js

--- Screen/MemorizationSession.js
+++ Screen/MemorizationSession.js
@@ -8,53 +8,52 @@ import {
   TouchableOpacity,
 } from 'react-native';
 import {FlatList} from 'react-native';
-import {Box, Circle, Col, Line, Padder, Row, ScaledText} from 'urip-rn-kit';
 import DropDownPicker from 'react-native-dropdown-picker';
 
-import QuranKemenag from 'quran-kemenag';
-import {Surah} from 'quran-kemenag/dist/intefaces';
-
-const MemorizationSession = () => {
+const RecordVoice = () => {
   var word = 'سَارِعِي لِلْمَجْدِ وَالْعَلْيَاء مَجِّدِي لِخَالِقِ السَّمَاء';
-  array = word.split(' ');
-  // var array2;
+  var array = word.split(' ');
+  var array2 = [];
   console.log(array);
   console.log(array[0]);
   console.log(array[1]);
   console.log(array[2]);
 
-  const data = ['First', 'Second', 'Third', 'Forth'];
+  const data = ['First', 'Second', 'Third', 'Forth','fifth'];
+  const clonedArr = [...data];
+
 
   const [show, setShow] = React.useState(false);
-  const toggleText = () => setShow(show => !show);
 
-  //   for (var i = 0; i < array.length; i++) {
-  //     var j = 0;
-  //     array2[j] = array[i];
-  //   }
+  const toggleText = () => {
+    setShow(show => !show);
+  };
+
+
+  for (var i = 1; i < clonedArr.length; i++) {
+    clonedArr[i++] = '_____';
+    console.log(clonedArr);
+  }
 
   return (
     <SafeAreaView>
       <Text style={{alignSelf: 'center'}}>{word}</Text>
 
-      {/*
-
-      
+      {/*       
       {array.map((array, index) => (
         <Text key={index}>{show ? array : '_ _ _ _ _ _ _ _'}</Text>
       ))}
-      
-       */}
+     */}
 
       <Text onPress={toggleText}>Toggle</Text>
       <FlatList
-        data={array}
-        renderItem={({item}) => (
-          <Text> {show ? item : '_ _ _ _ _ _ _ _'} </Text>
+        data={clonedArr}
+        renderItem={({item, index}) => (
+          <Text> {show ? item : '_______________'} </Text>
         )}
       />
     </SafeAreaView>
   );
 };
 
-export default MemorizationSession;
+export default RecordVoice;
