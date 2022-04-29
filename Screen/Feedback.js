abee5ba3eb6dfb360be4cac38e430f91d94de4f3
AlGhaliyahM <439201044@student.ksu.edu.sa>
2022-04-29 05:35:00 +0300

    Update Feedback.js

--- Screen/Feedback.js
+++ Screen/Feedback.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from 'react'
+import React, {useState, useEffect} from 'react';
 import {
   View,
   Text,
@@ -7,31 +7,31 @@ import {
   StatusBar,
   TouchableOpacity,
   Dimensions,
-} from 'react-native'
-import TitleStyles from '../Styles/Titles'
-import Refresh from '../assets/images/Refresh.svg'
-import Confetti from '../assets/images/Confetti.svg'
-import FocusAwareStatusBar from '../Components/FocusAwareStatusBar'
-import Close from '../assets/images/Close.svg'
-import {StackActions} from '@react-navigation/native'
-import firestore from '@react-native-firebase/firestore'
+} from 'react-native';
+import TitleStyles from '../Styles/Titles';
+import Refresh from '../assets/images/Refresh.svg';
+import Confetti from '../assets/images/Confetti.svg';
+import FocusAwareStatusBar from '../Components/FocusAwareStatusBar';
+import Close from '../assets/images/Close.svg';
+import {StackActions} from '@react-navigation/native';
+import firestore from '@react-native-firebase/firestore';
 import {UserInfoContext} from '../auth/UserInfoContext';
 
 const Feedback = ({navigation, route}) => {
-  const {width, height} = Dimensions.get('window')
+  const {width, height} = Dimensions.get('window');
   const {student} = React.useContext(UserInfoContext);
-  const [score, setScore] = useState(100)
-  const [trial, setTrial] = useState(100)
-  const [textBody, setTextBody] = useState()
-  const [textHead, setTextHead] = useState()
-  const mistakes = route.params.mistakesNum
-  const totalWords = route.params.totalWords
+  const [score, setScore] = useState(100);
+  const [trial, setTrial] = useState(100);
+  const [textBody, setTextBody] = useState();
+  const [textHead, setTextHead] = useState();
+  const mistakes = route.params.mistakesNum;
+  const totalWords = route.params.totalWords;
 
-  console.log(mistakes + '+' + totalWords)
+  console.log(mistakes + '+' + totalWords);
 
   useEffect(() => {
-    const mistakesRate = Math.round((mistakes / totalWords) * 100)
-    setScore(100 - mistakesRate)
+    const mistakesRate = Math.round((mistakes / totalWords) * 100);
+    setScore(100 - mistakesRate);
 
     //if its student text
     firestore()
@@ -40,25 +40,31 @@ const Feedback = ({navigation, route}) => {
       .get()
       .then(snapshot => {
         if (snapshot.exists) {
-          if (snapshot.get('Feedback') == null) { //student havent recited before
-            setTrial(1)
+          if (snapshot.get('Feedback') == null) {
+            //student havent recited before
+            setTrial(1);
             snapshot.ref.update({
-              Feedback: {score: 100 - mistakesRate, trial: 1,  mistakes: mistakes,},
-            })
+              Feedback: {
+                score: 100 - mistakesRate,
+                trial: 1,
+                mistakes: mistakes,
+              },
+            });
           } else {
-            if (snapshot.data().Feedback.trial < 3) {//student have recited before
-              setTrial(snapshot.data().Feedback.trial + 1)
+            if (snapshot.data().Feedback.trial < 3) {
+              //student have recited before
+              setTrial(snapshot.data().Feedback.trial + 1);
               snapshot.ref.update({
                 Feedback: {
                   score: 100 - mistakesRate,
                   trial: snapshot.data().Feedback.trial + 1,
                   mistakes: mistakes,
                 },
-              })
+              });
             }
           }
         }
-      })
+      });
 
     //if its instructor text
     firestore()
@@ -67,44 +73,46 @@ const Feedback = ({navigation, route}) => {
       .get()
       .then(snapshot => {
         if (snapshot.exists) {
-          const FeedbackField = snapshot.data().Feedback
-          if (snapshot.data().Feedback[student.id] == null) { //student havent recited before
-            setTrial(1)
+          const FeedbackField = snapshot.data().Feedback;
+          if (snapshot.data().Feedback[student.id] == null) {
+            //student havent recited before
+            setTrial(1);
             FeedbackField[student.id] = {
               score: 100 - mistakesRate,
               trial: 1,
               mistakes: mistakes,
-            }
+            };
             snapshot.ref.update({
               Feedback: FeedbackField,
-            })
-          } else { //student have recited before
-            var studentIndex = snapshot.data().Feedback[student.id]
+            });
+          } else {
+            //student have recited before
+            var studentIndex = snapshot.data().Feedback[student.id];
             if (studentIndex.trial < 3) {
-              setTrial(studentIndex.trial + 1)
+              setTrial(studentIndex.trial + 1);
               FeedbackField[student.id] = {
                 score: 100 - mistakesRate,
                 trial: studentIndex.trial + 1,
                 mistakes: mistakes,
-              }
+              };
               snapshot.ref.update({
                 Feedback: FeedbackField,
-              })
+              });
             }
           }
         }
-      })
-  }, [])
+      });
+  }, []);
 
   const EngToArabicNum = num => {
-    var str = '' + num
-    return str.replace(/\d/g, d => '٠١٢٣٤٥٦٧٨٩'[d])
-  }
+    var str = '' + num;
+    return str.replace(/\d/g, d => '٠١٢٣٤٥٦٧٨٩'[d]);
+  };
 
   return (
     <SafeAreaView
       style={{flex: 1, alignItems: 'center', backgroundColor: '#DAE2E9'}}>
-      <FocusAwareStatusBar backgroundColor='#DAE2E9' barStyle='dark-content' />
+      <FocusAwareStatusBar backgroundColor="#DAE2E9" barStyle="dark-content" />
       <View
         style={{
           position: 'absolute',
@@ -112,10 +120,10 @@ const Feedback = ({navigation, route}) => {
         <Confetti />
       </View>
       <Close
-        height='40'
-        width='40'
+        height="40"
+        width="40"
         style={[{position: 'absolute', top: 40, left: 20, zIndex: 2}]}
-        onPress={() => navigation.dispatch(StackActions.pop(2))}
+        onPress={() => navigation.dispatch(StackActions.pop(3))}
       />
       <View style={{marginTop: 160, alignItems: 'center'}}>
         <View
@@ -165,6 +173,6 @@ const Feedback = ({navigation, route}) => {
         ) : null}
       </View>
     </SafeAreaView>
-  )
-}
-export default Feedback
+  );
+};
+export default Feedback;
